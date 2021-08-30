cf3a0803e98d918d6a1cff5d1f38fc4bc10ec009
SaeedYasin <saeedyasin@gmail.com>
2021-08-31 00:37:25 +0800

    [svelte] Add svelte template with sass support

---
+++ svelte-counter-example/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let name;
+</script>
+
+<h1>Hello {name}!</h1>
+
+<div>
+  <p>SASS is working!</p>
+</div>
+
+<style lang="scss">
+  $color: red;
+
+  h1 {
+    color: $color;
+  }
+
+  div {
+    background: green;
+
+    > p {
+      color: #fff;
+    }
+  }
+</style>
