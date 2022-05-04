685df2850a375b6fea71ca730349e51fa4c92253
Saeed Yasin <saeedyasin@gmail.com>
2022-05-04 18:16:20 +0800

    Add autoprefixer

--- sveltekit-counter-example/src/routes/index.svelte
+++ sveltekit-counter-example/src/routes/index.svelte
@@ -1,3 +1,12 @@
+<script context="module" lang="ts">
+  export const prerender = true;
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Sveltekit Counter Example" />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
